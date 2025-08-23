039753b8f57705d095207d2d2fb9e16f465f3e61
Daniyal Ahmed <da9033431@gmail.com>
2025-08-23 13:12:15 +0100

    data merging using map and set

--- Data Merging/index.js
+++ Data Merging/index.js
@@ -1,24 +1,24 @@
 export default function mergeData(sessions) {
-    const userMap = new Map();
-  
-    sessions.forEach(({ user, duration, equipment }) => {
-      if (!userMap.has(user)) {
-        userMap.set(user, { user, duration: 0, equipment: new Set() });
-      }
-  
-      const userData = userMap.get(user);
-      userData.duration += duration;
-  
-      equipment.forEach((item) => userData.equipment.add(item));
-    });
-  
-    return Array.from(userMap.values()).map((u) => ({
-      user: u.user,
-      duration: u.duration,
-      equipment: [...u.equipment].sort(), 
-    }));
-  }
-  
+  const userMap = new Map();
+
+  sessions.forEach(({ user, duration, equipment }) => {
+    if (!userMap.has(user)) {
+      userMap.set(user, { user, duration: 0, equipment: new Set() });
+    }
+
+    const userData = userMap.get(user);
+    userData.duration += duration;
+
+    equipment.forEach((item) => userData.equipment.add(item));
+    // console.log(userMap);
+  });
+
+  return Array.from(userMap.values()).map((u) => ({
+    user: u.user,
+    duration: u.duration,
+    equipment: [...u.equipment].sort(), 
+  }));
+}
 
 const data = [
   { user: 8, duration: 50, equipment: ["bench"] },
@@ -29,4 +29,4 @@ const data = [
   { user: 2, duration: 200, equipment: ["treadmill"] },
   { user: 2, duration: 200, equipment: ["bike"] },
 ];
-console.log(mergeData(data))
+console.log(mergeData(data));
