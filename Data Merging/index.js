07f25d690edc1fbcfe918685ebc5c2f6100bcbda
Daniyal Ahmed <da9033431@gmail.com>
2025-08-23 12:45:39 +0100

    data merging problem solved

---
+++ Data Merging/index.js
@@ -0,0 +1,32 @@
+export default function mergeData(sessions) {
+    const userMap = new Map();
+  
+    sessions.forEach(({ user, duration, equipment }) => {
+      if (!userMap.has(user)) {
+        userMap.set(user, { user, duration: 0, equipment: new Set() });
+      }
+  
+      const userData = userMap.get(user);
+      userData.duration += duration;
+  
+      equipment.forEach((item) => userData.equipment.add(item));
+    });
+  
+    return Array.from(userMap.values()).map((u) => ({
+      user: u.user,
+      duration: u.duration,
+      equipment: [...u.equipment].sort(), 
+    }));
+  }
+  
+
+const data = [
+  { user: 8, duration: 50, equipment: ["bench"] },
+  { user: 7, duration: 150, equipment: ["dumbbell"] },
+  { user: 1, duration: 10, equipment: ["barbell"] },
+  { user: 7, duration: 100, equipment: ["bike", "kettlebell"] },
+  { user: 7, duration: 200, equipment: ["bike"] },
+  { user: 2, duration: 200, equipment: ["treadmill"] },
+  { user: 2, duration: 200, equipment: ["bike"] },
+];
+console.log(mergeData(data))
